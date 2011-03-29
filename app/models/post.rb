1d96effd5058849bbf1b26b0f2f6b65e2a802bc5
Nickolay Yegorov <provdk@gmail.com>
2011-03-29 14:12:56 +0400

    Created basic sctructure of sections

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,4 @@
 class Post < ActiveRecord::Base
-  validates :message, :presence => true
-  belongs_to :stream
-  
-  has_attached_file :photo,
-      :styles => { :medium => "200x200>", :large => "1600x1600>" } 
+  belongs_to :stream  
+  has_attached_file :photo, :styles => { :medium => "200x200>", :large => "1600x1600>" } 
 end
